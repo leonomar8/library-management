174b1915451a7b22151aff4c065d63a7bc6692d5
omar leon <leonomar8@gmail.com>
2025-08-08 00:01:26 -0500

    Initial commit for new deploy

--- app/main.py
+++ app/main.py
@@ -8,8 +8,5 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # Include routers
-#app.include_router(user.router, prefix="/users", tags=["Users"])
-#app.include_router(book.router, prefix="/books", tags=["Books"])
-
 app.include_router(user.router)
 app.include_router(book.router)
---
+++ application.py
@@ -0,0 +1,2 @@
+# application.py at project root
+from app.main import app as application
