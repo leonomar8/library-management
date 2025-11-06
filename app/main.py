f7b0e068013635829c6c077d89273203284e09cb
Omar Leon <88417232+leonomar8@users.noreply.github.com>
2025-11-06 17:14:48 -0600

    add CORS functionality to main.py
    Added CORS middleware to allow all origins and methods.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 from app.routers import user, book
 
@@ -6,6 +7,15 @@ from app.routers import user, book
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Simple CORS for FrontEnd interaction: allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # <- allow any origin
+    allow_credentials=True,
+    allow_methods=["*"],   # <- allow GET, POST, PUT, DELETE, OPTIONS
+    allow_headers=["*"],   # <- allow all headers
+)
 
 # Include routers
 app.include_router(user.router)
